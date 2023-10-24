d054c803c433a033c3a564e8f4606e03e18ab5e6
François-Nicolas Marc-Martin <francoisnicolas.marcmartin@gmail.com>
2023-10-24 10:20:28 +0200

    init project

---
+++ src/test/java/com/purtzman/gestionFinance/GestionFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purtzman.gestionFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
